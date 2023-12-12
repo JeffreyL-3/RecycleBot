645521fc973c1261d1c16f990eb0989469710653
JeffreyL-3 <121074476+JeffreyL-3@users.noreply.github.com>
2023-12-12 17:55:57 -0500

    Add files via upload

--- engine.py
+++ engine.py
@@ -52,14 +52,14 @@ def query_recycling_info(image_path, town, state, object="object", personality="
         "model": "gpt-4-vision-preview",
         "messages": [
             {
-                "role": "system", "content": "You are a helpful local waste management director" + combinedLocation + " helping others decide if their object is recyclable. All answers must be in this format: [Yes/No][object name][How to do this]. Example 1: [Yes][paper][Just toss it into your recycling bin]. Example 2: [Yes, but...][phone][Don't throw it in the bin! You can recycle this by bringing it to your nearest recycling center.]. Example 3: [No][styrofoam container][No need to recycle. Just toss it in the trash!]. Phrase your recycling instructions in the style of " + personality + ".",
+                "role": "system", "content": "You are a helpful local waste management director" + combinedLocation + " helping others decide if their object is recyclable. Phrase your recycling instructions in the style of " + personality + " while maintaining complete accuracy. All answers must be in this format: [Yes/No][object name][How to do this]. Example 1: [Yes][paper][Just toss it into your recycling bin]. Example 2: [Yes, but...][phone][Don't throw it in the bin! You can recycle this by bringing it to your nearest recycling center.]. Example 3: [No][styrofoam container][No need to recycle. Just toss it in the trash!].",
             },
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": "Is this " + object + " recyclable" + combinedLocation + "? Be sure your answer is fully complete and accurate."
+                        "text": "Is this " + object + " recyclable" + combinedLocation + "?"
                     },
                     {
                         "type": "image_url",
